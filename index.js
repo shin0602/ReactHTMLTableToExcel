bafb882d877f036c1cbd5f5cec8f285fc7137b74
Shin <shin.sa2k@gmail.com>
2020-01-28 18:07:33 +0900

    [fix] windows edge bug

--- index.js
+++ index.js
@@ -85,9 +85,10 @@ var ReactHTMLTableToExcel = function (_Component) {
       if (window.navigator.msSaveOrOpenBlob) {
         var fileData = ['' + ('<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-mic' + 'rosoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta cha' + 'rset="UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:Exce' + 'lWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>' + '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></' + 'xml><![endif]--></head><body>') + table + '</body></html>'];
         var blobObject = new Blob(fileData);
-        document.getElementById('react-html-table-to-excel').click()(function () {
-          window.navigator.msSaveOrOpenBlob(blobObject, filename);
-        });
+        window.navigator.msSaveOrOpenBlob(blobObject, filename);
+        // document.getElementById(this.props.id).click()(() => {
+        //   window.navigator.msSaveOrOpenBlob(blobObject, filename);
+        // });
 
         return true;
       }
